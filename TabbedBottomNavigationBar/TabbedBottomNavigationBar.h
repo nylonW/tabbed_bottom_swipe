f744d9ded5eaf72e3ca3b5df6f9af285aff47d9b
Marcin Ślusarek <mrvrota@gmail.com>
2020-11-16 18:13:58 +0100

    Initial Commit

---
+++ TabbedBottomNavigationBar/TabbedBottomNavigationBar.h
@@ -0,0 +1,18 @@
+//
+//  TabbedBottomNavigationBar.h
+//  TabbedBottomNavigationBar
+//
+//  Created by Marcin Slusarek on 16/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TabbedBottomNavigationBar.
+FOUNDATION_EXPORT double TabbedBottomNavigationBarVersionNumber;
+
+//! Project version string for TabbedBottomNavigationBar.
+FOUNDATION_EXPORT const unsigned char TabbedBottomNavigationBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabbedBottomNavigationBar/PublicHeader.h>
+
+
